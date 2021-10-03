ba26c786ffec9f4e9e339c94df4eff113530fd44
Mac Sunde <msunde@haverford.edu>
2021-10-03 18:39:38 -0400

    player controler update

--- Assets/Scripts/DraggableObject.cs
+++ Assets/Scripts/DraggableObject.cs
@@ -7,6 +7,7 @@ public class DraggableObject : MonoBehaviour
     private Rigidbody2D rb;
 
     private Vector2 mouseOffset;
+    Transform previousParent;
 
     void Start()
     {
@@ -16,13 +17,14 @@ public class DraggableObject : MonoBehaviour
     private void OnMouseDown()
     {
         mouseOffset = rb.position - PhysicalMouse.instance.GetPos();
+        previousParent = transform.parent;
         transform.SetParent(PhysicalMouse.instance.transform);
         PhysicalMouse.instance.hj.connectedBody = rb;
     }
 
     private void OnMouseUp()
     {
-        transform.SetParent(null);
+        transform.SetParent(previousParent);
         PhysicalMouse.instance.hj.connectedBody = null;
     }
 }
